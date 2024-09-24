6013b8e83ffebc004a2423f488a749bcccc7013f
Maicon <133474721+SolanoGuedes@users.noreply.github.com>
2024-09-24 17:46:44 -0300

    Initial commit

---
+++ src/main/java/com/solanoguedes/OER/OerApplication.java
@@ -0,0 +1,13 @@
+package com.solanoguedes.OER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solanoguedes/OER/OerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solanoguedes.OER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
